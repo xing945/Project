8afb0b9cf4afc6d25a83a0e35b47b07b5c654582
xing <642743457@qq.com>
2020-09-09 20:33:43 +0800

    git相关指令

--- jq京东首屏/fs/fs.js
+++
@@ -1,4 +0,0 @@
-// 载入
-$('.fs-1').load('./fs/fs1/fs1.html');
-$('.fs-2').load('./fs/fs2/fs2.html');
-$('.fs-3').load('./fs/fs3/fs3.html');
--- jq京东首屏/js/index.js
+++
@@ -1,5 +0,0 @@
-// 载入 
-$('.shortcut').load('./shortcut/shortcut.html');
-$('.header').load('./header/header.html');
-$('.fs').load('./fs/fs.html');
-$('.seckill').load('./seckill/seckill.html');
